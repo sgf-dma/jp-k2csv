9d9d8275778ba83463946ea8fdd67f80b97ed75b
sgf <sgf.dma@gmail.com>
2017-09-09 17:13:56 +0300

    new: vim-table. Parse table into a `Map`.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,12 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Monoid
 import Control.Applicative
 import qualified Data.Text              as T
+import qualified Data.Text.IO           as T
 import qualified Data.Attoparsec.Text   as A
+import qualified Data.Map               as M
 
 
 cellStart :: Char -> Bool
@@ -48,6 +51,9 @@ cellRow2 = T.concat <$> ((some $ whenNotP (A.string " | ") wordWspace)
 rowLine :: A.Parser [T.Text]
 rowLine = some cellLine <* A.string "|" <* A.takeTill A.isEndOfLine <* A.endOfLine
 
+rowLineN :: A.Parser (M.Map Int T.Text)
+rowLineN    = M.fromList . zip [1..] <$> rowLine
+
 -- | Separator row.
 sepRow :: A.Parser [T.Text]
 sepRow = some (A.string "+" *> A.takeWhile1 (== '-')) <* A.string "+" <* A.takeTill A.isEndOfLine <* A.endOfLine
@@ -58,14 +64,37 @@ headSepRow = some (A.string "+" *> A.takeWhile1 (== '=')) <* A.string "+" <* A.t
 -- | Multi-line table row. Newline at the end is /required/.
 -- FIXME: Make it not required. May be replace `endOfLine` with some parser
 -- `tokEnd`, which will match with `endOfInput` too.
+-- FIXME: Validate, that separator and row lengthes match.
 row :: A.Parser [[T.Text]]
 row     = some rowLine <* sepRow
+
+rowN :: A.Parser (M.Map Int T.Text)
+rowN    = (foldr (\x z -> M.unionWith (\l r -> l <> "\n" <> r) x z) M.empty <$> some rowLineN) <* sepRow
+
+{--- | Verified by length row.
+rowV :: A.Parser [[T.Text]]
+rowV    = do
+    rs <- some rowLine
+    ss <- sepRow-}
 
 headerRow :: A.Parser [[T.Text]]
 headerRow     = some rowLine <* headSepRow
 
+unlines' :: T.Text -> T.Text -> T.Text
+unlines' x y    = x <> "\n" <> y
+
+headerRowN :: A.Parser (M.Map Int T.Text)
+--headerRowN    = (foldr (\x z -> M.unionWith (\l r -> l <> "\n" <> r) x z) M.empty <$> some rowLineN) <* headSepRow
+headerRowN    = sepRow *> (M.unionsWith unlines' <$> some rowLineN) <* headSepRow
+
+
 table :: A.Parser [[[T.Text]]]
 table       = (:) <$> headerRow <*> some row
+
+--tableN :: A.Parser (M.Map Int (M.Map Int T.Text))
+tableN :: A.Parser (M.Map (Int, Int) T.Text)
+tableN  = (M.unions . map (\(r, mr) -> M.mapKeys (\c -> (r, c)) mr) . zip [1..]) <$>
+            ((:) <$> headerRowN <*> some rowN)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
