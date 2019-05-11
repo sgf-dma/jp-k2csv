562deda2ece4c87144c0ada9f9eeb14df0e0cb33
sgf <sgf.dma@gmail.com>
2019-05-11 15:24:50 +0300

    new: k2csv. Expect files in current directory. Use absolute pathes.

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main where
 import Data.Csv
 import qualified Data.ByteString.Lazy   as BL
 import qualified Data.Map               as M
+import           System.Directory
+import           System.FilePath
 
 import qualified Sgf.Data.Text.Table    as T
 import Sgf.Jp
@@ -14,7 +16,12 @@ import Sgf.Data.Text.OldTable
 
 main :: IO ()
 main = do
-    m <- T.decodeFileL "../words-mnn.txt" >>= either
+    cwd <- getCurrentDirectory >>= makeAbsolute
+    let wordsFile = cwd </> "words-mnn.txt"
+        conjFile  = cwd </> "conjugations.txt"
+        kanaFile  = cwd </> "kana.txt"
+
+    m <- T.decodeFileL wordsFile >>= either
         (\e -> error $ "Can't parse JWords table " ++ e)
         (return . buildMap number)
     checkMap m
@@ -23,14 +30,14 @@ main = do
     writeMap "words.csv"   m
     writeMap "words-ndsc.csv"   (M.map (map JWordN) m)
 
-    mconj <- T.decodeFileL "../conjugations.txt" >>= either
+    mconj <- T.decodeFileL conjFile >>= either
         (\e -> error $ "Can't parse JConj table " ++ e)
         (return . buildMap conjNumber)
     checkMap mconj
     writeMap "conj.csv" mconj
     writeMap "conj-full.csv" (M.map (map JConjF) mconj)
 
-    kw <- readFile "../kana.txt"
+    kw <- readFile kanaFile
     let ks = concatMap fst (parseAll kw) :: [JKana]
     BL.writeFile "kana.csv" (encode ks)
 
