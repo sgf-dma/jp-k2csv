e8b95e823b1fd91b04a94ba4ae5acc45e2d63fd8
sgf <sgf.dma@gmail.com>
2019-10-27 22:21:19 +0300

    gen_anki. Switch marker to S.

--- gen_anki_filter.hs
+++ gen_anki_filter.hs
@@ -4,7 +4,7 @@ import Data.List
 ns = take 50 $ [1..]
 
 kanji_wout_refs :: Show a => [a] -> [String]
-kanji_wout_refs = map (\x -> "(ref:*M" ++ show x ++ "-K* -ref:*M" ++ show x ++ "-K-R*)")
+kanji_wout_refs = map (\x -> "(ref:*S" ++ show x ++ "-K* -ref:*S" ++ show x ++ "-K-R*)")
 
 triples :: [a] -> [[a]]
 triples (x0 : y0 : zs) = foldr (\x z y w -> (w : y : [x]) : z x y) (\y w -> [[w, y]]) zs y0 x0
